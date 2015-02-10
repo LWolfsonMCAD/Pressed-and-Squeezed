25ffe6e03780ea0ecbddb719b64dc0fb9848c336
Lisa Wolfson <lwolfson@mcad.edu>
2015-02-09 21:08:03 -0600

    Added comments for future reference.

--- functions.php
+++ functions.php
@@ -1,6 +1,11 @@
 <?php
 
+// register the main navigation
+
 register_nav_menus();
+
+
+// register the sidebar-widgets
 
 register_sidebar(array(
 		'before_widget' => '<div class="sidebar">',
@@ -9,11 +14,16 @@ register_sidebar(array(
 		'after_title' => '</div>',
 	));
 
+
+// change the length of the post excerpts
+
 function custom_excerpt_length( $length ) {
 	return 30;
 }
 add_filter( 'excerpt_length', 'custom_excerpt_length', 999 );
 
+
+// change the ellipse to "Read More" at the end of the post excerpts
 
 function new_excerpt_more( $more ) {
 	return ' <a class="read-more" href="'. get_permalink( get_the_ID() ) . '">' . __('Read More', 'your-text-domain') . '</a>';
--- index.php
+++ index.php
@@ -1,12 +1,17 @@
 <?php get_header(); ?>
 <div class="clearfix">
+
+<!-- Main body of the page -->	
 		<section class="main">
 			<img src="http://localhost:8888/wp-content/themes/pressed/images/hero.jpg" />
-			
+
+<!-- WordPress Loop -->			
 			<?php if (have_posts()) : while (have_posts()) : the_post();?>
 		
 			<a href="<?php the_permalink();?>"><h1><?php the_title_attribute(); ?></h1></a>				
 				<section>
+	
+<!-- Currently displaying excerpts of posts -->
 				<?php the_excerpt(); ?>
 				
 				<!-- <p>Pressed and Squeezed is a full-service WordPress shop. We create custom themes and CMSes for our clients.</p>
@@ -20,6 +25,7 @@
 			<?php endif; ?>		
 		</section>
 
+<!-- Register sidebar -->
 		<?php get_sidebar(); ?>
 </div>		
 <?php get_footer(); ?>
